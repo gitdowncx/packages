bb3b19608a9d43ba0c67b9519987dabb3a3912e3
philip <>
2025-07-22 00:17:44 +0800

    新增歷史紀錄腳本與驗證執行指令腳本

---
+++ scripts/history.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+whoami_txt=$(who am i)
+login_ip=$(echo "$whoami_txt" | awk -F"[()]" '{print $2}')
+login_user=$(echo "$whoami_txt" | awk '{print $1}')
+login_tty=$(echo "$whoami_txt" | awk '{print $2}')
+
+touch /opt/logs/.histlog
+chmod 666 /opt/logs/.histlog
+
+> ~/.bash_history
+
+export PROMPT_COMMAND='{
+  cmd=$(history 1 | { read x c; echo "$c"; });
+  pid=$$;
+  pname=$(ps -p $pid -o comm=);
+
+  echo "$(date "+%b %e %T") $(hostname) USER=$(whoami) ; TTY='"$login_tty"' ; LOGIN_IP='"$login_ip"' ; PWD=$(pwd) ; COMMAND=$cmd ; PROCESS=$pname ; PID=$pid ;"
+} >> /opt/logs/.histlog'
+
+if [ -z "$(readonly -p | grep PROMPT_COMMAND)" ]; then
+  readonly PROMPT_COMMAND
+fi
