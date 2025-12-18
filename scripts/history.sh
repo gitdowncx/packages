656d9cb12179fcc96743068a9a009979cac6a415
philip <>
2025-12-18 11:38:52 +0800

    新增nginx-1.29.3、更新腳本

--- scripts/history.sh
+++ scripts/history.sh
@@ -12,9 +12,5 @@ export PROMPT_COMMAND='{
   pid=$$;
   pname=$(ps -p $pid -o comm=);
 
-  echo "$(date +"%Y-%m-%dT%H:%M:%S%:z") $(hostname) USER=$(whoami) ; TTY='"$login_tty"' ; LOGIN_IP='"$login_ip"' ; PWD=$(pwd) ; COMMAND=$cmd ; PROCESS=$pname ; PID=$pid ;"
+  echo "$(date +"%Y-%m-%dT%H:%M:%S%:z") $(hostname) USER=$(whoami) ; TTY='"$login_tty"' ; LOGIN_IP='"$login_ip"' ; PROCESS=$pname ; PID=$pid ; COMMAND=$cmd ; PWD=$(pwd) ;"
 } >> /opt/logs/.histlog'
-
-if [ -z "$(readonly -p | grep PROMPT_COMMAND)" ]; then
-  readonly PROMPT_COMMAND
-fi
